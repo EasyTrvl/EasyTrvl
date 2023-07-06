9e04931fcc4360f98d0aa10a39bd8fa0d51f1077
EasyTrvl <138775520+EasyTrvl@users.noreply.github.com>
2023-07-06 12:22:59 +0200

    Create README.m

---
+++ README.m
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Voyagez à travers le monde</title>
+  <style>
+    /* Styles CSS pour la mise en page */
+    body {
+      font-family: Arial, sans-serif;
+      margin: 0;
+      padding: 0;
+    }
+
+    .header {
+      background-color: #333;
+      color: #fff;
+      padding: 20px;
+      text-align: center;
+    }
+
+    .hero-image {
+      background-image: url("background.jpg");
+      background-size: cover;
+      background-position: center;
+      height: 500px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      text-align: center;
+    }
+
+    .hero-text {
+      color: #fff;
+      font-size: 36px;
+      font-weight: bold;
+      text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+    }
+
+    .cta-button {
+      background-color: #ff8c00;
+      color: #fff;
+      padding: 10px 20px;
+      font-size: 20px;
+      text-decoration: none;
+      border-radius: 4px;
+    }
+
+    .cta-button:hover {
+      background-color: #ffaf4d;
+    }
+
+    .features {
+      padding: 40px;
+      text-align: center;
+    }
+
+    .feature {
+      display: inline-block;
+      margin: 20px;
+    }
+
+    .feature-icon {
+      font-size: 48px;
+      color: #333;
+    }
+
+    .feature-title {
+      font-size: 24px;
+      margin-top: 10px;
+      margin-bottom: 10px;
+    }
+
+    .feature-description {
+      color: #777;
+    }
+
+    .footer {
+      background-color: #333;
+      color: #fff;
+      padding: 20px;
+      text-align: center;
+    }
+  </style>
+</head>
+<body>
+  <header class="header">
+    <h1>Voyagez à travers le monde</h1>
+    <p>Découvrez de nouveaux horizons et créez des souvenirs inoubliables</p>
+  </header>
+
+  <section class="hero-image">
+    <div class="hero-text">
+      <h2>Explorez les merveilles de notre planète</h2>
+      <p>Plongez-vous dans des cultures fascinantes, admirez des paysages à couper le souffle et créez des souvenirs qui dureront toute une vie.</p>
+      <a href="destinations.html" class="cta-button">Commencez votre voyage</a>
+    </div>
+  </section>
+
+  <section class="features">
+    <div class="feature">
+      <span class="feature-icon">🌍</span>
+      <h3 class="feature-title">Destinations exotiques</h3>
+      <p class="feature-description">Découvrez des destinations uniques, des plages tropicales aux jungles mystérieuses.</p>
+    </div>
+    <div class="feature">
+      <span class="feature-icon">✈️</span>
+      <h3 class="feature-title">Vols abordables</h3>
+      <p class="feature-description">Trouvez les meilleurs prix sur les vols pour réaliser votre rêve de voyage.</p>
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Voyagez à travers le monde</title>
+  <style>
+    /* Styles CSS pour la mise en page */
+    body {
+      font-family: Arial, sans-serif;
+      margin: 0;
+      padding: 0;
+    }
+
+    .header {
+      background-color: #333;
+      color: #fff;
+      padding: 20px;
+      text-align: center;
+    }
+
+    .hero-image {
+      background-image: url("background.jpg");
+      background-size: cover;
+      background-position: center;
+      height: 500px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      text-align: center;
+    }
+
+    .hero-text {
+      color: #fff;
+      font-size: 36px;
+      font-weight: bold;
+      text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+    }
+
+    .cta-button {
+      background-color: #ff8c00;
+      color: #fff;
+      padding: 10px 20px;
+      font-size: 20px;
+      text-decoration: none;
+      border-radius: 4px;
+    }
+
+    .cta-button:hover {
+      background-color: #ffaf4d;
+    }
+
+    .features {
+      padding: 40px;
+      text-align: center;
+    }
+
+    .feature {
+      display: inline-block;
+      margin: 20px;
+    }
+
+    .feature-icon {
+      font-size: 48px;
+      color: #333;
+    }
+
+    .feature-title {
+      font-size: 24px;
+      margin-top: 10px;
+      margin-bottom: 10px;
+    }
+
+    .feature-description {
+      color: #777;
+    }
+
+    .footer {
+      background-color: #333;
+      color: #fff;
+      padding: 20px;
+      text-align: center;
+    }
+  </style>
+</head>
+<body>
+  <header class="header">
+    <h1>Voyagez à travers le monde</h1>
+    <p>Découvrez de nouveaux horizons et créez des souvenirs inoubliables</p>
+  </header>
+
+  <section class="hero-image">
+    <div class="hero-text">
+      <h2>Explorez les merveilles de notre planète</h2>
+      <p>Plongez-vous dans des cultures fascinantes, admirez des paysages à couper le souffle et créez des souvenirs qui dureront toute une vie.</p>
+      <a href="destinations.html" class="cta-button">Commencez votre voyage</a>
+    </div>
+  </section>
+
+  <section class="features">
+    <div class="feature">
+      <span class="feature-icon">🌍</span>
+      <h3 class="feature-title">Destinations exotiques</h3>
+      <p class="feature-description">Découvrez des destinations uniques, des plages tropicales aux jungles mystérieuses.</p>
+    </div>
+    <div class="feature">
+      <span class="feature-icon">✈️</span>
+      <h3 class="feature-title">Vols abordables</h3>
+      <p class="feature-description">Trouvez les meilleurs prix sur les vols pour réaliser votre rêve de voyage.</p>
